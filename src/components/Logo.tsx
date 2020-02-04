95d47ea1c026908b30127a3f27f187a4f4f967c0
ffalt <ffalty@gmail.com>
2020-02-04 14:43:16 +0100

    fix(image): remove fast image option from image

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import FastImage from 'react-native-fast-image';
 import {Image, ImageStyle, StyleProp} from 'react-native';
 import {IMAGE_LOGO} from '../style/images';
 
@@ -9,7 +8,6 @@ export default class Logo extends React.PureComponent<{ size: number, style?: St
 			<Image
 				style={[this.props.style, {height: this.props.size, width: this.props.size}]}
 				source={IMAGE_LOGO}
-				resizeMode={FastImage.resizeMode.contain}
 			/>
 		);
 	}
