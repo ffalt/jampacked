c310ae53ff985667d3478eefd219875fd0ec60b8
ffalt <ffalty@gmail.com>
2020-09-02 16:30:45 +0200

    feat(player): play from cache

--- src/services/media-cache.ts
+++ src/services/media-cache.ts
@@ -1,24 +1,53 @@
 import {TrackEntry} from './types';
 import dataService from './data';
 import {AudioFormatType} from './jam';
-import RNBackgroundDownloader from 'react-native-background-downloader';
+import RNBackgroundDownloader, {DownloadTask} from 'react-native-background-downloader';
 import RNFS from 'react-native-fs';
+// @ts-ignore
+import DownloadNotification from 'react-native-download-notification';
+
+
+type DNotificationIntentHandler = () => void;
+
+interface DNotification {
+	updateProgress(progress: number): void;
+
+	setStateCompleted(): void;
+
+	setStateCancelled(): void;
+
+	setStateFailed(): void;
+
+	setExtraInfo(data: string): void;
+
+	getExtraInfo(): string;
+
+	getId(): string;
+
+	register(intentHandler: DNotificationIntentHandler): void;
+
+	unregister(): void;
+}
 
 export class MediaCache {
+	tasks: Array<DownloadTask> = [];
+
+	// private notification?: DNotification;
 
 	async init(): Promise<void> {
 		const lostTasks = await RNBackgroundDownloader.checkForExistingDownloads();
-		for (const task of lostTasks) {
-			console.log(`Task ${task.id} was found!`);
-			task.begin((expectedBytes) => {
-				console.log(task.id, `Going to download ${expectedBytes} bytes!`);
-			}).progress((percent) => {
-				console.log(task.id, `Downloaded: ${percent * 100}%`);
-			}).done(() => {
-				console.log(task.id, 'Download is done!');
-			}).error((error) => {
-				console.log(task.id, 'Download canceled due to error: ', error);
-			});
+		this.tasks = lostTasks || [];
+		await this.start();
+	}
+
+	intentHandler(intent: string): void {
+		// intent is a String, depending on the notification state could be:
+		//   'cancel' if download is on going
+		//   'open' if it is set to completed
+		//   'dismiss' if the user dismisses it
+		if (intent === 'cancel') {
+			// a reference of the notification tapped is binded
+			// this.setStateCancelled();
 		}
 	}
 
@@ -39,61 +68,54 @@ export class MediaCache {
 					url: dataService.jam.stream.streamUrl({id: t.id, format: AudioFormatType.mp3}, !headers),
 					destination: this.pathInCache(t.id),
 					headers
-				})
-					.begin((expectedBytes) => {
-						console.log(task.id, `Going to download ${expectedBytes} bytes!`);
-					}).progress((percent) => {
-						console.log(task.id, `Downloaded: ${percent * 100}%`);
-					}).done(() => {
-						console.log(task.id, 'Download is done!');
-					}).error((error) => {
-						console.log(task.id, 'Download canceled due to error: ', error);
-					});
-				task.resume();
+				});
+				task.pause();
+				this.tasks.push(task);
 			}
 		}
-
-		/*
-
-		let task = RNBackgroundDownloader.download({
-	id: 'file123',
-	url: 'https://link-to-very.large/file.zip'
-	destination: `${RNBackgroundDownloader.directories.documents}/file.zip`,
-	headers: {
-	    Authorization: 'Bearer 2we$@$@Ddd223'
-	}
-}).begin((expectedBytes) => {
-	console.log(`Going to download ${expectedBytes} bytes!`);
-}).progress((percent) => {
-	console.log(`Downloaded: ${percent * 100}%`);
-}).done(() => {
-	console.log('Download is done!');
-}).error((error) => {
-	console.log('Download canceled due to error: ', error);
-});
-		 */
-		// let task = RNBackgroundDownloader.download({
-		// 	id: 'file123',
-		// 	url: 'https://link-to-very.large/file.zip'
-		// 	destination: `${RNBackgroundDownloader.directories.documents}/file.zip`
-		// }).begin((expectedBytes) => {
-		// 	console.log(`Going to download ${expectedBytes} bytes!`);
-		// }).progress((percent) => {
-		// 	console.log(`Downloaded: ${percent * 100}%`);
-		// }).done(() => {
-		// 	console.log('Download is done!');
-		// }).error((error) => {
-		// 	console.log('Download canceled due to error: ', error);
-		// });
-
+		await this.start();
+		// .pause();
+		// this.connectToTask(task);
 		// Pause the task
 		// 		task.pause();
-
 		// Resume after pause
 		// 		task.resume();
-
 		// Cancel the task
 		// 		task.stop();
 	}
 
+	private connectToTask(task: DownloadTask, notification: DNotification): void {
+		task
+			.begin((expectedBytes) => {
+				console.log(task.id, `Going to download ${expectedBytes} bytes!`);
+				notification.setExtraInfo(`Total: ${expectedBytes}`)
+			})
+			.progress((percent) => {
+				notification.updateProgress(percent * 100);
+				console.log(task.id, `Downloaded: ${percent * 100}%`);
+			})
+			.done(() => {
+				this.tasks = this.tasks.filter(t => t !== task);
+				console.log(task.id, 'Download is done!');
+				notification.setStateCompleted();
+				this.start();
+			})
+			.error((error) => {
+				console.log(task.id, 'Download canceled due to error: ', error);
+				notification.setStateCancelled();
+			});
+	}
+
+	private async start(): Promise<void> {
+		if (this.tasks.length > 0) {
+			console.log('start tasks', this.tasks.length);
+			const task = this.tasks[0];
+			const notification = await DownloadNotification.create(
+				`Audio ${task.id}`,
+				this.intentHandler
+			);
+			this.connectToTask(task, notification);
+			task.resume();
+		}
+	}
 }
