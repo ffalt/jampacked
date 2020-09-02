ec6cdd942c44b2d3cc8bec189ead98b644b6a7d1
ffalt <ffalty@gmail.com>
2020-09-02 16:41:59 +0200

    fix(favicon): mute while loading

--- src/components/FavIcon.tsx
+++ src/components/FavIcon.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {ActivityIndicator, StyleProp, StyleSheet, TouchableOpacity, View, ViewStyle} from 'react-native';
+import {StyleProp, StyleSheet, TouchableOpacity, View, ViewStyle} from 'react-native';
 import {staticTheme, useTheme} from '../style/theming';
 import {ThemedIcon} from './ThemedIcon';
 import {JamObjectType} from '../services/jam';
@@ -33,6 +33,7 @@ export const FavIcon: React.FC<{ id?: string; objType: JamObjectType; style?: St
 	}, [faved]);
 
 	const isFaved = (fav?.timestamp || 0) > 0;
+	const iconName = isFaved ? 'heart-full' : 'heart-empty';
 
 	const handleToggleFav = (): void => {
 		if (id && fav) {
@@ -44,18 +45,13 @@ export const FavIcon: React.FC<{ id?: string; objType: JamObjectType; style?: St
 				});
 		}
 	};
-	if (loading || !faved) {
-		return (<ActivityIndicator/>);
-	}
-
-	if (!fav) {
+	if (loading || !fav) {
 		return (
 			<View style={[styles.button, style]}>
-				<ThemedIcon name="heart-empty" size={styles.buttonIcon.fontSize} color={theme.muted}/>
+				<ThemedIcon name={iconName} size={styles.buttonIcon.fontSize} color={theme.muted}/>
 			</View>
 		);
 	}
-	const iconName = isFaved ? 'heart-full' : 'heart-empty';
 	return (
 		<TouchableOpacity style={[styles.button, style]} onPress={handleToggleFav}>
 			<ThemedIcon name={iconName} size={styles.buttonIcon.fontSize}/>
