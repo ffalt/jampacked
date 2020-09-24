b8d1920ab94a0205976ca6a6d7bfb88b5b6f478a
ffalt <ffalty@gmail.com>
2020-09-24 21:53:21 +0200

    feat(track item): double tab for play, single for action sheet

--- src/components/TrackItem.tsx
+++ src/components/TrackItem.tsx
@@ -1,50 +1,69 @@
 import React, {useCallback} from 'react';
-import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {State, TapGestureHandler, TapGestureHandlerStateChangeEvent} from 'react-native-gesture-handler';
 import {JamPlayer} from '../services/player';
 import {ThemedText} from './ThemedText';
 import {TrackEntry} from '../services/types';
 import {sharedStyles} from '../style/shared';
+import {staticTheme} from '../style/theming';
 
 const styles = StyleSheet.create({
 	trackListNumber: {
-		flex: 1
+		flex: 1,
+		maxWidth: 90,
+		paddingRight: staticTheme.paddingLarge
 	},
 	trackListTitle: {
-		flex: 7
+		flex: 8
 	},
 	trackListRuntime: {
+		maxWidth: 90,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		paddingLeft: staticTheme.paddingLarge,
+		paddingRight: staticTheme.paddingLarge,
 		flex: 1
 	}
 });
 
 export const TrackItem: React.FC<{ track: TrackEntry, showMenu?: (item: TrackEntry) => void; }> = React.memo(({track, showMenu}) => {
+	const doubleTap = React.useRef(React.createRef<TapGestureHandler>().current);
 
-	const playTrack = useCallback((): void => {
-		JamPlayer.playTrack(track)
-			.catch(e => console.error(e));
-	}, [track]);
-
-	const popupMenu = useCallback((): void => {
-		if (showMenu) {
-			showMenu(track);
+	const onSingleTapped = useCallback((event: TapGestureHandlerStateChangeEvent): void => {
+		if (event.nativeEvent.state === State.ACTIVE) {
+			if (showMenu) {
+				showMenu(track);
+			}
 		}
 	}, [showMenu, track]);
 
+	const onDoubleTapped = useCallback((event: TapGestureHandlerStateChangeEvent): void => {
+		if (event.nativeEvent.state === State.ACTIVE) {
+			JamPlayer.playTrack(track)
+				.catch(e => console.error(e));
+		}
+	}, [track]);
+	//TODO: implement opacity on press
 	return (
-		<TouchableOpacity
-			onPress={playTrack}
-			onLongPress={popupMenu}
-			style={sharedStyles.item}
-		>
-			<View style={[sharedStyles.itemSectionLeft, styles.trackListNumber]}>
-				<ThemedText style={sharedStyles.itemFooterText}>{track.trackNr}</ThemedText>
-			</View>
-			<View style={[sharedStyles.itemContent, styles.trackListTitle]}>
-				<ThemedText style={sharedStyles.itemText} numberOfLines={2}>{track.title}</ThemedText>
-			</View>
-			<View style={[sharedStyles.itemSectionRight, styles.trackListRuntime]}>
-				<ThemedText style={sharedStyles.itemFooterText}>{track.duration}</ThemedText>
-			</View>
-		</TouchableOpacity>
+		<TapGestureHandler
+			waitFor={doubleTap}
+			onHandlerStateChange={onSingleTapped}>
+			<TapGestureHandler
+				ref={doubleTap}
+				onHandlerStateChange={onDoubleTapped}
+				numberOfTaps={2}>
+				<View style={sharedStyles.item}>
+					<View style={[sharedStyles.itemSectionLeft, styles.trackListNumber]}>
+						<ThemedText style={[sharedStyles.itemFooterText, sharedStyles.itemFooterTextRight]} numberOfLines={1}>{track.trackNr}</ThemedText>
+					</View>
+					<View style={[sharedStyles.itemContent, styles.trackListTitle]}>
+						<ThemedText style={sharedStyles.itemText} numberOfLines={2}>{track.title}</ThemedText>
+					</View>
+					<View style={[sharedStyles.itemSectionRight, styles.trackListRuntime]}>
+						<ThemedText style={[sharedStyles.itemFooterText, sharedStyles.itemFooterTextRight]} numberOfLines={1}>{track.duration}</ThemedText>
+					</View>
+				</View>
+			</TapGestureHandler>
+		</TapGestureHandler>
 	);
 });
