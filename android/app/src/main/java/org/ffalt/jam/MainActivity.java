04e40c4abd06453b05947cf3ba7471637bd56fa5
ffalt <ffalty@gmail.com>
2020-12-20 14:13:08 +0100

    chore(deps): bump react-native-bootsplash to 3.1.2

--- android/app/src/main/java/org/ffalt/jam/MainActivity.java
+++ android/app/src/main/java/org/ffalt/jam/MainActivity.java
@@ -1,12 +1,12 @@
 package org.ffalt.jam;
 
 import android.os.Bundle;
+import android.content.res.Configuration;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-
 import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
@@ -22,9 +22,14 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-     RNBootSplash.init(this, R.style.AppTheme);
+     // RNBootSplash.init(this, R.style.AppTheme);
      super.onCreate(null);
-  }
+     int drawableId = (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
+                     ? R.drawable.bootsplash_dark
+                     : R.drawable.bootsplash_light; //Default light theme
+
+     RNBootSplash.init(drawableId, MainActivity.this);
+ }
 
   @Override
 	protected ReactActivityDelegate createReactActivityDelegate() {
