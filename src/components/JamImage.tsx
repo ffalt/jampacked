55e61533b1ad6c533f5e782978cfca3b1e413540
ffalt <ffalty@gmail.com>
2021-07-31 19:26:21 +0200

    feat(image): use webp

--- src/components/JamImage.tsx
+++ src/components/JamImage.tsx
@@ -3,10 +3,11 @@ import FastImage, {ImageStyle} from 'react-native-fast-image';
 import {StyleProp} from 'react-native';
 import {useAuth} from '../services/auth';
 import {staticTheme} from '../style/theming';
+import {ImageFormatType} from '../services/jam';
 
 export const JamImage: React.FC<{ id: string, size?: number, requestSize?: number, style?: StyleProp<ImageStyle> }> = React.memo(({id, requestSize, size, style}) => {
 	const auth = useAuth();
-	const source = React.useMemo(() => auth.imgSource(id, requestSize || staticTheme.thumbMedium), [auth, id, requestSize]);
+	const source = React.useMemo(() => auth.imgSource(id, requestSize || staticTheme.thumbMedium, ImageFormatType.webp), [auth, id, requestSize]);
 
 	if (!source || !source.uri) {
 		return (<></>);
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react';
 import dataService from './data';
-import {Jam} from './jam';
+import {ImageFormatType, Jam} from './jam';
 import FastImage, {Source} from 'react-native-fast-image';
 
 export interface Auth {
@@ -8,7 +8,7 @@ export interface Auth {
 	user?: Jam.SessionUser,
 	currentUserID: () => string;
 	currentUserName: () => string;
-	imgSource: (id: string, size: number) => Source | undefined;
+	imgSource: (id: string, size: number, format?: ImageFormatType) => Source | undefined;
 	login: (server: string, name: string, password: string) => Promise<void>;
 	logout: () => Promise<void>;
 }
@@ -18,13 +18,13 @@ export const defaultAuth: Auth = {
 	user: undefined,
 	currentUserID: (): string => dataService.currentUserID,
 	currentUserName: (): string => dataService.currentUserName,
-	imgSource: (id: string, size: number): Source | undefined => {
+	imgSource: (id: string, size: number, format?: ImageFormatType): Source | undefined => {
 		if (!id || !dataService.jam.auth.isLoggedIn()) {
 			return;
 		}
 		const headers = dataService.currentUserToken ? {Authorization: `Bearer ${dataService.currentUserToken}`} : undefined;
 		return {
-			uri: dataService.jam.image.imageUrl({id, size}, !headers),
+			uri: dataService.jam.image.imageUrl({id, size, format}, !headers),
 			headers,
 			priority: FastImage.priority.normal
 		};
