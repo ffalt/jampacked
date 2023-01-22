bf6e9f8a359b9fffeb0b329f513172199a4d97cd
ffalt <ffalty@gmail.com>
2023-01-22 16:33:52 +0100

    chore(deps): update ios build

--- ios/jam/AppDelegate.mm
+++ ios/jam/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import "RNBootSplash.h" // <- add the header import
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -7,8 +8,14 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"jam";
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  self.initialProps = @{};
+
   [super application:application didFinishLaunchingWithOptions:launchOptions];
+
   [RNBootSplash initWithStoryboard:@"BootSplash" rootView:self.window.rootViewController.view];
+
   return YES;
 }
 
