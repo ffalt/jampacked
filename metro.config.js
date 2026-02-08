d544bbe5d78148f79bab161da1e55ada541dab3b
ffalt <ffalty@gmail.com>
2026-02-08 17:50:19 +0100

    fix(metro.config): prevent Metro from resolving nested node_modules in local-path dependencies

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,6 @@
 // eslint-disable-next-line unicorn/prefer-module
+const path = require("path");
+// eslint-disable-next-line unicorn/prefer-module
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
 /**
@@ -7,7 +9,17 @@ const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 	*
 	* @type {import("@react-native/metro-config").MetroConfig}
 	*/
-const config = {};
+const config = {
+	resolver: {
+		// Prevent Metro from resolving packages from nested node_modules
+		// inside local-path dependencies (e.g. react-native-track-player)
+		blockList: [
+			new RegExp(
+				path.resolve(__dirname, "node_modules/react-native-track-player/node_modules/.*").replace(/[/\\]/g, "[/\\\\]")
+			),
+		],
+	},
+};
 
 // eslint-disable-next-line unicorn/prefer-module
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
