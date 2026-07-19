d7ea6a8dc0df0e4a065c5086f13db752eaedfa2d
ffalt <ffalty@gmail.com>
2026-07-19 14:18:45 +0200

    chore(babel): add react-native-worklets plugin to config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line unicorn/prefer-module
 module.exports = {
 	presets: ["module:@react-native/babel-preset"],
-	plugins: ["@babel/plugin-transform-export-namespace-from"]
+	plugins: ["@babel/plugin-transform-export-namespace-from", "react-native-worklets/plugin"]
 };
